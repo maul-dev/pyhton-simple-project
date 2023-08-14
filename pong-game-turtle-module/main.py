ebb2c1bf73739c97a7b89dd8e6430162e5403f3d
Faisal Maulana <maulanafaisal.7666@gmai.com>
2023-08-14 20:17:28 +0700

    fixing class paddle , make scoreboard but not working:(

--- pong-game-turtle-module/main.py
+++ pong-game-turtle-module/main.py
@@ -19,12 +19,12 @@ def binding_key(screen, f_paddle, s_paddle):
     screen.onkeypress(fun=s_paddle.down, key="s")
 
 
-first_paddle_position = (-600, 0)
-second_paddle_position = (600, 0)
+first_paddle_position = [(-600, 0), (-600, 20), (-600, 40), (-600, -20), (-600, -40)]
+second_paddle_position = [(600, 0), (600, 20), (600, 40), (600, -20), (600, -40)]
 
 screen_window = create_screen()
-first_paddle = Paddle(first_paddle_position)
-second_paddle = Paddle(second_paddle_position)
+first_paddle = Paddle(first_paddle_position, (-60, 300))
+second_paddle = Paddle(second_paddle_position, (60, 300))
 ball = Ball()
 binding_key(screen_window, first_paddle, second_paddle)
 
@@ -33,9 +33,14 @@ while game_is_on:
     screen_window.update()
     time.sleep(0.1)
     ball.move()
-    if first_paddle.distance(ball) < 30 or second_paddle.distance(ball) < 30:
+    if first_paddle.collision(ball) or second_paddle.collision(ball):
         ball.collision_paddle()
-    if ball.ycor() > 330 or ball.ycor() < -330:
+
+    if ball.ycor() > 345 or ball.ycor() < -345:
         ball.collision_wall()
+
+    if ball.xcor() > 1280/2 or ball.xcor() < -1280/2:
+        first_paddle.score +=1
+        ball.refresh()
 
 screen_window.exitonclick()
--- pong-game-turtle-module/paddle.py
+++ pong-game-turtle-module/paddle.py
@@ -1,24 +1,46 @@
 from turtle import Turtle
-from random import randint
+import random
 
 
-class Paddle(Turtle):
-    def __init__(self, position):
-        super().__init__()
-        self.shape("square")
-        self.shapesize(stretch_len=5)
-        self.color("white")
-        self.penup()
-        self.setheading(90)
-        self.setposition(position)
+class Paddle:
+    # def __init__(self, position):
+    #     super().__init__()
+    #     self.shape("square")
+    #     self.shapesize(stretch_len=5)
+    #     self.color("white")
+    #     self.penup()
+    #     self.setheading(90)
+    def __init__(self, position, scoreboard_position):
+        self.paddle = self.create_paddle(position)
+        self.score = 0
+        self.scoreboard = Scoreboard(self.score, scoreboard_position)
+
+    @staticmethod
+    def create_paddle(position):
+        pad = []
+        for pos in position:
+            new_paddle = Turtle("square")
+            new_paddle.penup()
+            new_paddle.color("white")
+            new_paddle.goto(pos)
+            pad.append(new_paddle)
+        return pad
 
     def up(self):
-        self.setheading(90)
-        self.forward(20)
+        for pad in self.paddle:
+            pad.setheading(90)
+            pad.forward(20)
 
     def down(self):
-        self.setheading(270)
-        self.forward(20)
+        for pad in self.paddle:
+            pad.setheading(270)
+            pad.forward(20)
+
+    def collision(self, ball) -> bool:
+        for pad in self.paddle:
+            if pad.distance(ball) < 30:
+                return True
+        return False
 
 
 class Ball(Turtle):
@@ -28,7 +50,19 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.shapesize(stretch_wid=0.8, stretch_len=0.8)
-        self.setheading(randint(0, 360))
+        self.random_launch()
+
+    def refresh(self):
+        self.setposition(0, 0)
+        self.random_launch()
+
+    def random_launch(self):
+        direction = ["left", "right"]
+        rand_dir = random.choice(direction)
+        if rand_dir == "left":
+            self.setheading(random.randint(120, 240))
+        else:
+            self.setheading(random.randint(300, 420))
 
     def move(self):
         self.forward(20)
@@ -38,3 +72,16 @@ class Ball(Turtle):
 
     def collision_wall(self):
         self.setheading(360 - self.heading())
+
+
+class Scoreboard(Turtle):
+    def __init__(self, score, position):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(position)
+        self.display_score(score)
+
+    def display_score(self, score):
+        self.write(arg=f"{score}", align="center", font=('Arial', 18, 'normal'))
